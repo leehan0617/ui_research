7e2ce33be90ae8de6424d9cc23597eb3d68932b9
hanbin <leehan0617@naver.com>
2024-08-19 16:55:55 +0900

    저압케이블 로직 수정

--- src/states/low_cable_selector.js
+++ src/states/low_cable_selector.js
@@ -1,6 +1,6 @@
 import { selector } from "recoil";
 import { areaState } from "./atom";
-import { densityState, devAreaState, scaleConstantState, singleAdjState } from "./input_selector";
+import { densityState, devAreaState, scaleConstantState, singleAdjState, commonAdjState } from "./input_selector";
 import { lowCablePrice } from "@/constants/price";
 
 const lowCableState = selector({
@@ -10,11 +10,12 @@ const lowCableState = selector({
         const density = get(densityState);
         const devArea = get(devAreaState);
         const singleAdj = get(singleAdjState);
+        const commonAdj = get(commonAdjState);
         const currentScale = get(scaleConstantState);
         const { lowCable, densityAvg } = currentScale;
         const { unitCount, cable240, cable120, cable70 } = lowCable;
         const densityConstant = densityAvg > density ? 1 - (densityAvg - density) / densityAvg : 1;
-        return { area, unitCount, cable240, cable120, cable70, devArea, singleAdj, densityConstant };
+        return { area, unitCount, cable240, cable120, cable70, devArea, singleAdj, densityConstant, commonAdj };
     }
 });
 
@@ -22,10 +23,12 @@ export const cable240State = selector({
     key: "cable240State",
     get: ({ get }) => {
         const lowCable = get(lowCableState);
-        const { area, unitCount, cable240, devArea, singleAdj, densityConstant } = lowCable;
+        const { area, unitCount, cable240, devArea, singleAdj, densityConstant, commonAdj } = lowCable;
         const scale = Math.round(unitCount * cable240 * 1000) / 1000;
         // const count = Math.round(scale * area / 1000 * 100) / 100;
-        const count = Math.round(devArea * unitCount * cable240 * singleAdj * densityConstant / 1000 / 3 * 1000) / 1000;
+        // const count = Math.round(devArea * unitCount * cable240 * singleAdj * densityConstant / 1000 / 3 * 1000) / 1000;
+        const adj = (1+(singleAdj*commonAdj)) * densityConstant;
+        const count = Math.round(area * scale / 1000 * adj * 100) / 100;
         const companyPrice = lowCablePrice?.company;
         const customerPrice = lowCablePrice?.customer;
         return { scale, count, companyPrice, customerPrice };
@@ -36,10 +39,12 @@ export const cable120State = selector({
     key: "cable120State",
     get: ({ get }) => {
         const lowCable = get(lowCableState);
-        const { area, unitCount, cable120, devArea, singleAdj, densityConstant } = lowCable;
+        const { area, unitCount, cable120, devArea, singleAdj, densityConstant, commonAdj } = lowCable;
         const scale = Math.round(unitCount * cable120 * 1000) / 1000;
         // const count = Math.round(scale * area / 1000 * 100) / 100;
-        const count = Math.round(devArea * unitCount * cable120 * singleAdj * densityConstant / 1000 / 3 * 1000) / 1000;
+        // const count = Math.round(devArea * unitCount * cable120 * singleAdj * densityConstant / 1000 / 3 * 1000) / 1000;
+        const adj = (1+(singleAdj*commonAdj)) * densityConstant;
+        const count = Math.round(area * scale / 1000 / 3 * adj * 100) / 100;
         return { scale, count };
     }
 });

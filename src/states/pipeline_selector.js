adaebbc8bf6a59cd2728a7f23e7fa1626cfb8365
hanbin <leehan0617@naver.com>
2024-08-20 15:02:40 +0900

    파이프라인 로직 수정

--- src/states/pipeline_selector.js
+++ src/states/pipeline_selector.js
@@ -16,7 +16,8 @@ const pipelineState = selector({
         const { p175x9, p175x6, p175x4, p175x2 } = pipeline;
         const { p150x9, p150x6, p150x4, p150x2 } = pipeline;
         const { p100x9, p100x6, p100x4, p100x2 } = pipeline;
-        const densityConstant = densityAvg > density ? 1 - (densityAvg - density) / densityAvg : 1; 
+        let densityConstant = densityAvg > density ? 1 - (densityAvg - density) / densityAvg : 1; 
+        densityConstant = densityConstant * (1-0.1*commonAdj);
         return {
             area, unitCount, p175, p150, p100,
             p175x9, p175x6, p175x4, p175x2,
@@ -33,11 +34,11 @@ export const p175State = selector({
         const pipeline = get(pipelineState);
         const { area, unitCount, p175, p175x9, p175x6, p175x4, p175x2, devArea, commonAdj, densityConstant } = pipeline;
         // const scale = Math.round(unitCount * p175 * 1000) / 1000;
-        const scale = unitCount * p175 * commonAdj * densityConstant;
-        const count9 = Math.round(scale * p175x9 * devArea / 1000 * 100) / 100;
-        const count6 = Math.round(scale * p175x6 * devArea / 1000 * 100) / 100;
-        const count4 = Math.round(scale * p175x4 * devArea / 1000 * 100) / 100;
-        const count2 = Math.round(scale * p175x2 * devArea / 1000 * 100) / 100;
+        const scale = Math.round(unitCount * p175 * densityConstant * 1000) / 1000;
+        const count9 = Math.round(scale * p175x9 * area / 1000 * 100) / 100;
+        const count6 = Math.round(scale * p175x6 * area / 1000 * 100) / 100;
+        const count4 = Math.round(scale * p175x4 * area / 1000 * 100) / 100;
+        const count2 = Math.round(scale * p175x2 * area / 1000 * 100) / 100;
         return { scale, count9, count6, count4, count2 }
     }
 });
@@ -47,12 +48,11 @@ export const p150State = selector({
     get: ({ get }) => {
         const pipeline = get(pipelineState);
         const { area, unitCount, p150, p150x9, p150x6, p150x4, p150x2, devArea, commonAdj, densityConstant } = pipeline;
-        // const scale = Math.round(unitCount * p150 * 1000) / 1000;
-        const scale = unitCount * p150 * commonAdj * densityConstant;
-        const count9 = Math.round(scale * p150x9 * devArea / 1000 * 100) / 100;
-        const count6 = Math.round(scale * p150x6 * devArea / 1000 * 100) / 100;
-        const count4 = Math.round(scale * p150x4 * devArea / 1000 * 100) / 100;
-        const count2 = Math.round(scale * p150x2 * devArea / 1000 * 100) / 100;
+        const scale = Math.round(unitCount * p150 * densityConstant * 1000) / 1000;
+        const count9 = Math.round(scale * p150x9 * area / 1000 * 100) / 100;
+        const count6 = Math.round(scale * p150x6 * area / 1000 * 100) / 100;
+        const count4 = Math.round(scale * p150x4 * area / 1000 * 100) / 100;
+        const count2 = Math.round(scale * p150x2 * area / 1000 * 100) / 100;
         return { scale, count9, count6, count4, count2 }
     }
 });
@@ -62,12 +62,11 @@ export const p100State = selector({
     get: ({ get }) => {
         const pipeline = get(pipelineState);
         const { area, unitCount, p100, p100x9, p100x6, p100x4, p100x2, devArea, commonAdj, densityConstant } = pipeline;
-        // const scale = Math.round(unitCount * p100Í * 1000) / 1000;
-        const scale = unitCount * p100 * commonAdj * densityConstant;
-        const count9 = Math.round(scale * p100x9 * devArea / 1000 * 100) / 100;
-        const count6 = Math.round(scale * p100x6 * devArea / 1000 * 100) / 100;
-        const count4 = Math.round(scale * p100x4 * devArea / 1000 * 100) / 100;
-        const count2 = Math.round(scale * p100x2 * devArea / 1000 * 100) / 100;
+        const scale = Math.round(unitCount * p100 * densityConstant * 1000) / 1000;
+        const count9 = Math.round(scale * p100x9 * area / 1000 * 100) / 100;
+        const count6 = Math.round(scale * p100x6 * area / 1000 * 100) / 100;
+        const count4 = Math.round(scale * p100x4 * area / 1000 * 100) / 100;
+        const count2 = Math.round(scale * p100x2 * area / 1000 * 100) / 100;
         return { scale, count9, count6, count4, count2 }
     }
 });
